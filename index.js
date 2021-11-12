1806f0990608dbdbfe268861dd0accaa395a4764
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-11-12 10:40:40 -0500

    completed callback section

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function summation(num) {
   for(let i = 0; i <= num; i++){
     sum += i
   }
-  return sum
+  return sum;
   }
 summation(4);
 
@@ -64,7 +64,7 @@ const zooAnimals = [
     arr.forEach(item =>{
       displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`);
     })
-    return displayNames
+    return displayNames;
   }
 
   // console.log(animalNames(zooAnimals));
@@ -117,30 +117,30 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a, b, cb){
+    return cb(a,b);
   }
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a,b){
+    return a + b;
   }
 
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
+function multiply(a,b){
+   return a * b;
   }
 
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(a,b){
+   return `Hello ${a} ${b}, nice to meet you!`;
   }
   
   
@@ -148,7 +148,7 @@ function greeting(/*Your Code Here */){
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
   // console.log(consume(2, 2, add)); // 4
   // console.log(consume(10, 16, multiply)); // 160
-  // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+  console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // 🐴🐴🐴 Topic 3: Prototypes 🐴🐴🐴 //
 //🐴🐴🐴 Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge. 🐴🐴🐴
